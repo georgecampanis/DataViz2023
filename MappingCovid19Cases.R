5274bf0ae2c912063cf0516b9eae86e69b395ff5
George Campanis <georgecampanis@gmail.com>
2023-12-01 10:59:10 -0400

    Added SQL Connection

--- MappingCovid19Cases.R
+++ MappingCovid19Cases.R
@@ -1,44 +1,26 @@
 
-# install.packages(c("sf", "rgdal","geojsonio","spdply","rmapshaper","magrittr"))
-# 
-install.packages("sf")
+install.packages("RODBC")
+library(RODBC)
+library(tidyverse)
 
+dbconnection = odbcDriverConnect('driver={SQL Server};server=ML-RefVm-361229;database=Covid19;trusted_connection=true')
 
-# library(sf) # the base package manipulating shapes
-# #library(rgdal) # geo data abstraction library
-# library(geojsonio) # geo json input and output
-# library(spdplyr) # the `dplyr` counterpart for shapes
-# # library(rmapshaper) # the package that allows geo shape transformation
-# # library(magrittr) # data wrangling
-# # library(dplyr)
-# # library(tidyr)
-# # library(ggplot2)
+# cases
+covid.casedata <- sqlQuery(dbconnection,paste("select * from cases;"))
 
 
 
-# Resources
-# https://cran.r-project.org/web/packages/geojsonR/vignettes/the_geojsonR_package.html
-# https://teng.pub/technical/2020/1/7/drawing-canada-maps-in-r
-# https://mgimond.github.io/Spatial/reading-and-writing-spatial-data-in-r.html
+#pop data
+covid.popdata <- sqlQuery(dbconnection,paste("select * from PopulationData;"))
 
 
-# Stats Can boundary Files=> https://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/bound-limit-2011-eng.cfm
+odbcClose(dbconnection)
 
-#   
-# .shp file is where the data live in
-library(sf)
-library(ggplot2)
-library(dplyr)
+View(covid.casedata)# now join pop data
 
-#https://r-spatial.github.io/sf/index.html
-# https://r.geocompx.org/index.html
-
-s.sf <- sf::st_read("~\MappingData\gpr_000b11a_e.shp")
-
-canada_sf <- geodata::gadm("Canada", path = tempdir(), resolution = 1) |> st_as_sf()
-canada_sf <- st_simplify(canada_sf, dTolerance = 1000)
-# https://epsg.io/3348
-canada_sf <- st_transform(canada_sf, 3348) 
+max_cases_by_prov <- covid.casedata %>% group_by(Province) %>% 
+  summarize(cases_max= max(CumulativeCases)) %>% 
+inner_join(covid.popdata, by = "Province") 
 
 
 
